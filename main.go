6bb403c70d73dc16cc9d40b25b7e087746e5f249
Richard Hull <rm_hull@yahoo.co.uk>
2026-03-30 11:51:08 +0100

    feat: implement basic Ebiten game loop
    Refactored the main entry point to use a dedicated `Game` struct.
    This setup implements the required `ebiten.Game` interface methods:
    
    *   `Update`: Handles game logic updates.
    *   `Draw`: Renders the screen with a black background.
    *   `Layout`: Sets the fixed resolution to 1024x768.
    
    ```mermaid
    sequenceDiagram
        participant E as Ebiten Engine
        participant G as Game Struct
        E->>G: Layout()
        loop Every Frame
            E->>G: Update()
            E->>G: Draw()
        end
    ```

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"image/color"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-func main() {
-	g := &struct{}{}
+type Game struct{}
 
-	// ebiten.SetFullscreen(true)
-	ebiten.SetWindowSize(1024, 768)
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.Black)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 1024, 768
+}
+
+func main() {
+	g := &Game{}
 	err := ebiten.RunGame(g)
 	if err != nil {
 		panic(err)
